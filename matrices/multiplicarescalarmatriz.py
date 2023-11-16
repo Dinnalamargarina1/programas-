0069d2ea8466bfcf774234d671bb33ba2cd21d18
Dinnalamargarina1 <150369412+Dinnalamargarina1@users.noreply.github.com>
2023-11-16 12:06:42 -0500

    Update multiplicarescalarmatriz.py
    avance

--- matrices/multiplicarescalarmatriz.py
+++ matrices/multiplicarescalarmatriz.py
@@ -1,1 +1,32 @@
+def multiplicar_por_escalar(matriz, escalar):
+    if not matriz:
+        return None  # Devolver None si la matriz está vacía
 
+    filas = len(matriz)
+    columnas = len(matriz[0])
+    resultado = []
+
+    for i in range(filas):
+        fila_resultado = []
+        for j in range(columnas):
+            elemento_resultado = matriz[i][j] * escalar
+            fila_resultado.append(elemento_resultado)
+        resultado.append(fila_resultado)
+
+    return resultado
+
+# Ejemplo de uso:
+mi_matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+escalar = 2
+
+resultado = multiplicar_por_escalar(mi_matriz, escalar)
+if resultado is not None:
+    for fila in resultado:
+        print(fila)
+else:
+    print("La matriz está vacía.")

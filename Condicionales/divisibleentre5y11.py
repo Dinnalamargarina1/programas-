f28f2a1ca455fc9858a2769ab91a2afce3a6b6be
Dinnalamargarina1 <150369412+Dinnalamargarina1@users.noreply.github.com>
2023-11-16 11:34:25 -0500

    Update divisibleentre5y11.py
    actualizar

--- Condicionales/divisibleentre5y11.py
+++ Condicionales/divisibleentre5y11.py
@@ -1,1 +1,11 @@
-
+# 4. Escriba un programa para verificar si un número es divisible o no por 5 y 11.
+x= int(input("digite un número "))
+    
+if x%5==0:
+    print("el número es divisible por 5")
+else:
+    print("el número no es divisible por 5")
+if x%11==0:
+    print("el número es divisible por 11")
+else:
+    print("el número no es divisible por 11")

3c275fabfd816daf0e744d586fbb4260dbceb754
Dinnalamargarina1 <150369412+Dinnalamargarina1@users.noreply.github.com>
2023-11-16 11:36:12 -0500

    Update letraono.py
    avanvce

--- Condicionales/letraono.py
+++ Condicionales/letraono.py
@@ -1,1 +1,6 @@
-
+# 7. Escriba un programa para verificar si un caracter es una letra del alfabeto o no.
+caracter = input("Ingrese un carácter: ")
+if caracter.isalpha():
+    print(f"{caracter} es una letra del alfabeto.")
+else:
+    print(f"{caracter} no es una letra del alfabeto.")

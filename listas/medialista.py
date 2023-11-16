b0503171ae8fdf6d1d12a7a36c2d3d3282f547a7
Dinnalamargarina1 <150369412+Dinnalamargarina1@users.noreply.github.com>
2023-11-16 11:42:54 -0500

    Update medialista.py
    avances

--- listas/medialista.py
+++ listas/medialista.py
@@ -1,1 +1,23 @@
+#Dada una lista de números hallar la media 
+def calcular_media(lista):
+    if not lista:  # Verificar si la lista está vacía
+        return None
+    
+    suma = 0
+    cantidad = 0
 
+    for numero in lista:
+        suma += numero  # Sumar cada número a la suma total
+        cantidad += 1   # Contar la cantidad de números
+    
+    media = suma / cantidad  # Calcular la media dividiendo la suma entre la cantidad de números
+    return media
+
+# Ejemplo de uso:
+numeros = [13, 27, 8, 45, 19, 62]
+media = calcular_media(numeros)
+
+if media is not None:
+    print("La media de la lista es:", media)
+else:
+    print("La lista está vacía")

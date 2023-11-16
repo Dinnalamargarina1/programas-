99634e75ff97c4f55f4e373a28e5bb35fc45fe77
Dinnalamargarina1 <150369412+Dinnalamargarina1@users.noreply.github.com>
2023-11-16 12:09:45 -0500

    Update dimensiónmatriz.py
    avance

--- matrices/dimensiónmatriz.py
+++ matrices/dimensiónmatriz.py
@@ -1,1 +1,19 @@
+#Escriba una función que indique las dimesiones de una matriz 
+def dimensiones_matriz(matriz):
+    if not matriz or not isinstance(matriz, list) or not isinstance(matriz[0], list):
+        return 0, 0  # Devolver 0 para filas y columnas si la matriz está vacía o no es válida
+    
+    filas = len(matriz)
+    columnas = len(matriz[0]) if filas > 0 else 0
+    
+    return filas, columnas
 
+# Ejemplo de uso:
+mi_matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+num_filas, num_columnas = dimensiones_matriz(mi_matriz)
+print(f"La matriz tiene {num_filas} filas y {num_columnas} columnas.")

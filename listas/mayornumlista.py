ac4cd4fed8f554c3faff4eaf3867da2f803cd9b1
Dinnalamargarina1 <150369412+Dinnalamargarina1@users.noreply.github.com>
2023-11-16 11:42:30 -0500

    Update mayornumlista.py
    avances

--- listas/mayornumlista.py
+++ listas/mayornumlista.py
@@ -1,1 +1,17 @@
+#Dada una lista de números encontrar el número mayor.
+def encontrar_mayor(lista):
+    if not lista:  # Verificar si la lista está vacía
+        return None
 
+    maximo = lista[0]  # Asignar el primer número como el máximo inicialmente
+
+    for numero in lista:
+        if numero > maximo:
+            maximo = numero  # Actualizar el número máximo si encontramos uno mayor
+    
+    return maximo
+
+# Ejemplo de uso:
+numeros = [13, 27, 8, 45, 19, 62]
+mayor = encontrar_mayor(numeros)
+print("El número mayor es:", mayor)

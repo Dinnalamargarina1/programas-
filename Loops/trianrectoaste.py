e6458f339b0badb90aa2f9966d4535e2c35257cd
Dinnalamargarina1 <150369412+Dinnalamargarina1@users.noreply.github.com>
2023-11-16 11:40:24 -0500

    Update trianrectoaste.py
    avance

--- Loops/trianrectoaste.py
+++ Loops/trianrectoaste.py
@@ -1,1 +1,9 @@
-
+#Escriba un programa para mostrar el patrón como el triángulo de ángulo recto con un asterisco.
+#El patrón como:
+#*
+#**
+#***
+#****
+num_filas = int(input("Ingrese el número de filas para el triángulo: "))
+for i in range(1, num_filas + 1):
+    print('*' * i)
